5518171824a4cdca554aef33a1bf4f0481747880
andrzcavalcante <andrzcavalcante@gmail.com>
2023-10-02 10:22:02 -0300

    subindo projeto para prod

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(/* {
+    origin: 'http://localhost:3000',
+  } */);
   const config = new DocumentBuilder()
     .setTitle('IMusics')
     .setDescription('Software de músicas')
